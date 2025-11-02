b5597fc70eaf03234462045c3b218b74973dcb0d
Lina El Boudadi <Lina.Elboudadi@emsi-edu.ma>
2025-11-02 23:57:06 +0100

    Step 5: Added in llm LlmClientForGuideTouristique class

---
+++ src/main/java/ma/emsi/elboudadi/tp3jakartaee/llm/LlmClientForGuideTouristique.java
@@ -0,0 +1,22 @@
+package ma.emsi.elboudadi.tp3jakartaee.llm;
+
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+
+public class LlmClientForGuideTouristique {
+    private static final String API_KEY =
+            System.getenv("GEMINI_API_KEY"); // clé API dans variable d’environnement
+    private final GuideTouristique guideTouristique;
+    public LlmClientForGuideTouristique() {
+        var model = GoogleAiGeminiChatModel.builder()
+                .apiKey(API_KEY)
+                .modelName("gemini-1.5-flash")
+                .build();
+        guideTouristique = AiServices.create(GuideTouristique.class, (ChatLanguageModel) model);
+    }
+    public String getInfos(String lieu) {
+        return guideTouristique.getInfosTouristiques(lieu);
+    }
+}
