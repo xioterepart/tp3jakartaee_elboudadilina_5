b4fb321ceb39e203c3d51e44e3060576fe610b12
Lina El Boudadi <Lina.Elboudadi@emsi-edu.ma>
2025-11-02 23:11:05 +0100

    Step 2: Test1 in GuideTouristiquResource

--- src/main/java/ma/emsi/elboudadi/tp3jakartaee/resources/GuideTouristiquResource.java
+++ src/main/java/ma/emsi/elboudadi/tp3jakartaee/resources/GuideTouristiquResource.java
@@ -1,4 +1,18 @@
 package ma.emsi.elboudadi.tp3jakartaee.resources;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+@Path("/guide")
 public class GuideTouristiquResource {
+
+    @GET
+    @Path("lieu/{ville_ou_pays}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String villeOuPays(@PathParam("ville_ou_pays") String lieu) {
+// pour commencer, renvoyez simplement la valeur du paramètre
+        return "\"" + lieu + "\""; // renvoie une string JSON-like (unsimple chaîne)
+    }
 }
