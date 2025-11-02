ebbf7ea64edb66f14c954d59a3ee02ab8aba0728
Lina El Boudadi <Lina.Elboudadi@emsi-edu.ma>
2025-11-02 22:46:24 +0100

    Step 0: Created project

---
+++ src/main/java/ma/emsi/elboudadi/tp3jakartaee/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.elboudadi.tp3jakartaee;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
